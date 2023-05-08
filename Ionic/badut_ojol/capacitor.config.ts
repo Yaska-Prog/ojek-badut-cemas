324a1b60992242f1f48fb189852252257e80744f
Yaska-Prog <christianleon442@gmail.com>
2023-05-08 19:45:46 +0700

    Initial ionic starter
    Everything works perfectly

---
+++ Ionic/badut_ojol/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'badut_ojol',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
