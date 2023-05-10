1860838d0139b52f590fad6339a8874b36eeb96f
jryand25 <jonathan.ryan.darmawan@gmail.com>
2023-05-10 16:22:39 +0700

    update sql

--- badut_ojol.sql
+++ badut_ojol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2023 at 02:54 PM
+-- Generation Time: May 10, 2023 at 11:22 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -31,11 +31,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `customers` (
   `id` int(11) NOT NULL,
   `username` varchar(200) DEFAULT NULL,
-  `password` varchar(200) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
   `nama_lengkap` varchar(200) DEFAULT NULL,
-  `tanggal_lahir` date DEFAULT NULL,
+  `tanggal_lahir` varchar(200) DEFAULT NULL,
   `saldo` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `username`, `password`, `nama_lengkap`, `tanggal_lahir`, `saldo`) VALUES
+(1, 'XQbOtQ==', '499040539588de80b9ffe324cdaa2caff85eb5e15a6ae35e5a92f5114cb1cb40', 'ZRDBuq6KUBCpyebY', 'HU+f6ffSCVPJhQ==', 0);
 
 -- --------------------------------------------------------
 
@@ -46,13 +53,20 @@ CREATE TABLE `customers` (
 CREATE TABLE `drivers` (
   `id` int(11) NOT NULL,
   `username` varchar(200) DEFAULT NULL,
-  `password` varchar(200) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
   `nama_lengkap` varchar(200) DEFAULT NULL,
-  `tanggal_lahir` date DEFAULT NULL,
+  `tanggal_lahir` varchar(200) DEFAULT NULL,
   `plat_nomor` varchar(200) DEFAULT NULL,
   `merk_kendaraan` varchar(200) DEFAULT NULL,
   `warna_kendaraan` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `drivers`
+--
+
+INSERT INTO `drivers` (`id`, `username`, `password`, `nama_lengkap`, `tanggal_lahir`, `plat_nomor`, `merk_kendaraan`, `warna_kendaraan`) VALUES
+(1, 'XQbOtes=', '499040539588de80b9ffe324cdaa2caff85eb5e15a6ae35e5a92f5114cb1cb40', 'ZRDBuq6KUBCpyebY', 'HU+f6ffSCVPJhQ==', 'Y06W4+imaw==', 'Vario', 'Merah');
 
 -- --------------------------------------------------------
 
@@ -76,12 +90,19 @@ CREATE TABLE `menu` (
 CREATE TABLE `merchants` (
   `id` int(11) NOT NULL,
   `username` varchar(200) DEFAULT NULL,
-  `password` varchar(200) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
   `nama_lengkap` varchar(200) DEFAULT NULL,
-  `tanggal_lahir` date DEFAULT NULL,
+  `tanggal_lahir` varchar(200) DEFAULT NULL,
   `nama_resto` varchar(200) DEFAULT NULL,
   `alamat` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `merchants`
+--
+
+INSERT INTO `merchants` (`id`, `username`, `password`, `nama_lengkap`, `tanggal_lahir`, `nama_resto`, `alamat`) VALUES
+(1, 'XQbOteg=', '499040539588de80b9ffe324cdaa2caff85eb5e15a6ae35e5a92f5114cb1cb40', 'ZRDBuq6KUBCpyebY', 'HU+f6ffSCVPJhQ==', 'XRrcr7XT', 'RR7DurSpVBye3ubE3MQ=');
 
 -- --------------------------------------------------------
 
@@ -192,19 +213,19 @@ ALTER TABLE `order_rides`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `drivers`
 --
 ALTER TABLE `drivers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `merchants`
 --
 ALTER TABLE `merchants`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
