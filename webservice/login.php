45942ce42bdf75572e8e57ec2c7a4044303fe6e9
jryand25 <jonathan.ryan.darmawan@gmail.com>
2023-05-11 11:49:39 +0700

    update login.php

--- webservice/login.php
+++ webservice/login.php
@@ -68,6 +68,33 @@
       $data['nama_lengkap'] = openssl_decrypt($data['nama_lengkap'], $method, $key, $options, $iv);
       $data['tanggal_lahir'] = openssl_decrypt($data['tanggal_lahir'], $method, $key, $options, $iv);
       $data['plat_nomor'] = openssl_decrypt($data['plat_nomor'], $method, $key, $options, $iv);
+
+      $status = "Selesai";
+  
+      $sql = "SELECT o.id AS id, o.alamat_jemput AS alamat_jemput, o.alamat_tujuan AS alamat_tujuan, o.jarak AS jarak, o.tarif AS tarif, o.status AS status, c.nama_lengkap AS customer_name, d.nama_lengkap as driver_name
+              FROM order_rides o
+              INNER JOIN customers c on o.customer_id = c.id
+              INNER JOIN drivers d on o.driver_id = d.id
+              WHERE d.id = ? AND o.status != ?";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("is", $data['id'], $status);
+      $stmt->execute();
+  
+      $result = $stmt->get_result();
+      if($result->num_rows > 0){
+        $order = $result->fetch_assoc();
+        $data['order_id'] = $order['id'];
+  
+        $order['alamat_jemput'] = openssl_encrypt($order['alamat_jemput'], $method, $key, $options, $iv);
+        $order['alamat_tujuan'] = openssl_encrypt($order['alamat_tujuan'], $method, $key, $options, $iv);
+        $order['customer_name'] = openssl_encrypt($order['customer_name'], $method, $key, $options, $iv);
+        $order['driver_name'] = openssl_encrypt($order['driver_name'], $method, $key, $options, $iv);
+  
+        $data['order'] = $order;
+      }
+      else{
+        $data['order_id'] = -1;
+      }
     }
   }
   else{
@@ -78,13 +105,15 @@
     $data['nama_lengkap'] = openssl_decrypt($data['nama_lengkap'], $method, $key, $options, $iv);
     $data['tanggal_lahir'] = openssl_decrypt($data['tanggal_lahir'], $method, $key, $options, $iv);
 
+    $status = "Selesai";
+
     $sql = "SELECT o.id AS id, o.alamat_jemput AS alamat_jemput, o.alamat_tujuan AS alamat_tujuan, o.jarak AS jarak, o.tarif AS tarif, o.status AS status, c.nama_lengkap AS customer_name, d.nama_lengkap as driver_name
             FROM order_rides o
             INNER JOIN customers c on o.customer_id = c.id
             INNER JOIN drivers d on o.driver_id = d.id
-            WHERE c.id = ?";
+            WHERE c.id = ? AND o.status != ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $data['id']);
+    $stmt->bind_param("is", $data['id'], $status);
     $stmt->execute();
 
     $result = $stmt->get_result();
